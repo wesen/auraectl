b840b1d13e2b12014a41a58eaf4a24e73911a479
Manuel Odendahl <wesen@ruinwesen.com>
2022-09-16 10:29:19 -0400

    Some more cleanup to get an aurae-d auraectl

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,51 @@
-/*===========================================================================*\
- *           MIT License Copyright (c) 2022 Kris Nóva <kris@nivenly.com>     *
- *                                                                           *
- *                ┏━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┓                *
- *                ┃   ███╗   ██╗ ██████╗ ██╗   ██╗ █████╗   ┃                *
- *                ┃   ████╗  ██║██╔═████╗██║   ██║██╔══██╗  ┃                *
- *                ┃   ██╔██╗ ██║██║██╔██║██║   ██║███████║  ┃                *
- *                ┃   ██║╚██╗██║████╔╝██║╚██╗ ██╔╝██╔══██║  ┃                *
- *                ┃   ██║ ╚████║╚██████╔╝ ╚████╔╝ ██║  ██║  ┃                *
- *                ┃   ╚═╝  ╚═══╝ ╚═════╝   ╚═══╝  ╚═╝  ╚═╝  ┃                *
- *                ┗━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━━┛                *
- *                                                                           *
- *                       This machine kills fascists.                        *
- *                                                                           *
-\*===========================================================================*/
+/* -------------------------------------------------------------------------- *\
+ *             Apache 2.0 License Copyright © 2022 The Aurae Authors          *
+ *                                                                            *
+ *                +--------------------------------------------+              *
+ *                |   █████╗ ██╗   ██╗██████╗  █████╗ ███████╗ |              *
+ *                |  ██╔══██╗██║   ██║██╔══██╗██╔══██╗██╔════╝ |              *
+ *                |  ███████║██║   ██║██████╔╝███████║█████╗   |              *
+ *                |  ██╔══██║██║   ██║██╔══██╗██╔══██║██╔══╝   |              *
+ *                |  ██║  ██║╚██████╔╝██║  ██║██║  ██║███████╗ |              *
+ *                |  ╚═╝  ╚═╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝ |              *
+ *                +--------------------------------------------+              *
+ *                                                                            *
+ *                         Distributed Systems Runtime                        *
+ *                                                                            *
+ * -------------------------------------------------------------------------- *
+ *                                                                            *
+ *   Licensed under the Apache License, Version 2.0 (the "License");          *
+ *   you may not use this file except in compliance with the License.         *
+ *   You may obtain a copy of the License at                                  *
+ *                                                                            *
+ *       http://www.apache.org/licenses/LICENSE-2.0                           *
+ *                                                                            *
+ *   Unless required by applicable law or agreed to in writing, software      *
+ *   distributed under the License is distributed on an "AS IS" BASIS,        *
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *   See the License for the specific language governing permissions and      *
+ *   limitations under the License.                                           *
+ *                                                                            *
+\* -------------------------------------------------------------------------- */
 
 extern crate core;
 
 use clap::*;
-use log::*;
 use syslog::*;
 
 const EXIT_OKAY: i32 = 0;
 //const EXIT_ERROR: i32 = 1;
 
-fn runtime() -> i32 {
-    return EXIT_OKAY;
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about=None)]
+struct Cli {
+    #[clap(short, long)]
+    verbose: bool,
 }
 
-fn runtime_environment() {
-    let name = "nova";
-
-    // Initialize the program
-    let matches = App::new("Nova")
-        .version("1.0")
-        .author("Kris Nóva <kris@nivenly.com>")
-        .about(name)
-        .arg(
-            Arg::with_name("verbose")
-                .short('v')
-                .long("verbose")
-                .help("Toggle the verbosity bit.") // With <3 from @togglebit
-                .takes_value(false),
-        )
-        .get_matches();
+fn run() -> i32 {
+    let args = Cli::parse();
+    let name = "auraectl";
 
     // The logger will log to stdout and the syslog by default.
     // We hold the opinion that the program is either "verbose"
@@ -50,7 +53,7 @@ fn runtime_environment() {
     //
     // Normal mode: Info, Warn, Error
     // Verbose mode: Debug, Trace, Info, Warn, Error
-    let logger_level = if matches.is_present("verbose") {
+    let logger_level = if args.verbose {
         log::Level::Trace
     } else {
         log::Level::Info
@@ -76,18 +79,10 @@ fn runtime_environment() {
         Err(e) => panic!("unable to connect to syslog: {:?}", e),
     };
 
-    // Initialize the program
-    info!("*");
-    info!("*");
-    info!("* Runtime environment initialized: {}", name);
-    info!("*  -> Syslog process name: {}", name);
-    debug!("* Runtime **debugging** enabled: {}", name);
-    info!("*");
-    info!("*");
+    EXIT_OKAY
 }
 
 fn main() {
-    runtime_environment();
-    let exit_code = runtime();
+    let exit_code = run();
     std::process::exit(exit_code);
 }
